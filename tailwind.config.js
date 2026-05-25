2d8abbf41c2c8a05286347d9725b14f40b353459
sadeghbiglar <sbmail555@gmail.com>
2026-05-25 23:05:17 +0330

    install vazirmatn font

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Vazirmatn', ...defaultTheme.fontFamily.sans],
             },
         },
     },
